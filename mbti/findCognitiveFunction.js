79e10c1c4d66b0bea3deafe464c6b9d75b4c7b74
pot <pot@sennalabs.com>
2021-01-04 22:43:51 +0700

    find extroverted and introverted function if attitude is P

--- mbti/findCognitiveFunction.js
+++ mbti/findCognitiveFunction.js
@@ -12,4 +12,12 @@ export function findCognitiveFunction(mbti) {
   let func1 = mbti[1];
   let func2 = mbti[2];
   const attr = mbti[3];
+
+  let extro = "";
+  let intro = "";
+
+  if (attr === "P") {
+    extro = func1 + "e";
+    intro = func2 + "i";
+  }
 }
