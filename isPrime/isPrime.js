8b1d8bcf8c7113eb4028733b3a6dedaa549ad0ec
pot <pot@sennalabs.com>
2021-01-04 17:16:14 +0700

    finish ja

--- isPrime/isPrime.js
+++ isPrime/isPrime.js
@@ -1,3 +1,11 @@
 export function isPrime(num) {
-  throw new Error('Not implemented')
+  if (num === 1) return false;
+
+  const round = Math.floor(Math.sqrt(num));
+
+  for (let i = 2; i <= round; i++) {
+    if (num % i === 0) return false;
+  }
+
+  return true;
 }
